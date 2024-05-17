d997fb2c8423079a05670a358082c4709bc4c6f6
Doguhan <DoguhanErolJob@gmail.com>
2024-05-17 15:49:41 +0300

    Project Start.

---
+++ src/main/java/com/example/UserNotesApp/UserNotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserNotesApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserNotesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UserNotesApp/UserNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserNotesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
